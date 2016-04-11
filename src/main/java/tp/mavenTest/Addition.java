0313d20baf2ed1896bed7cbb48601e1b59a63bd7
nomane <nomane.boulmerdj@gmail.com>
2016-04-12 00:52:17 +0200

    ajouter checkstyle

--- src/main/java/tp/mavenTest/Addition.java
+++ src/main/java/tp/mavenTest/Addition.java
@@ -1,9 +1,27 @@
 package tp.mavenTest;
 
+/**
+ * @author newman
+ *
+ */
+
+
 public class Addition {
+	
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	  public Long calculer(Long a, Long b) {
-	      return a+b;
+	      return a + b;
 	   }
+	  
+	  /**
+	   * 
+	   * @return
+	   */
 	   public Character lireSymbole() {
 	      return '-';
 	   }
